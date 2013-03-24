b4ad52855ad3e993d3f86c52457b892aee1a6b6f
Snowmanzzz <apple@Snowman.local>
2013-03-24 15:50:46 +0800

    xml parsing

--- Weather/WTTableViewController.h
+++ Weather/WTTableViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface WTTableViewController : UITableViewController
+@interface WTTableViewController : UITableViewController <NSXMLParserDelegate>
 
 - (IBAction)clear:(id)sender;
 
@@ -17,5 +17,11 @@
 - (IBAction)xmlTapped:(id)sender;
 - (IBAction)httpClientTapped:(id)sender;
 - (IBAction)apiTapped:(id)sender;
+
+@property(strong) NSMutableDictionary *xmlWeather; //package containing the complete response
+@property(strong) NSMutableDictionary *currentDictionary; //current section being parsed
+@property(strong) NSString *previousElementName;
+@property(strong) NSString *elementName;
+@property(strong) NSMutableString *outstring;
 
 @end
--- Weather/WTTableViewController.m
+++ Weather/WTTableViewController.m
@@ -12,6 +12,7 @@
 #import "NSDictionary+weather_package.h"
 #import "AFJSONRequestOperation.h"
 #import "AFPropertyListRequestOperation.h"
+#import "AFXMLRequestOperation.h"
 static NSString *const BaseURLString = @"http://www.raywenderlich.com/downloads/weather_sample/";
 
 
@@ -22,6 +23,8 @@ static NSString *const BaseURLString = @"http://www.raywenderlich.com/downloads/
 @end
 
 @implementation WTTableViewController
+
+
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -120,6 +123,26 @@ static NSString *const BaseURLString = @"http://www.raywenderlich.com/downloads/
 
 - (IBAction)xmlTapped:(id)sender
 {
+    NSString *weatherUrl = [NSString stringWithFormat:@"%@weather.php?format=xml",BaseURLString];
+    NSURL *url = [NSURL URLWithString:weatherUrl];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    AFXMLRequestOperation *operation = [AFXMLRequestOperation
+                                        XMLParserRequestOperationWithRequest:request
+                                        success:^(NSURLRequest *request, NSHTTPURLResponse *response, NSXMLParser *XMLParser) {
+                                            // the line below used to be commented out
+                                            self.xmlWeather = [NSMutableDictionary dictionary];
+                                            XMLParser.delegate = self;
+                                            [XMLParser setShouldProcessNamespaces:YES];
+                                            [XMLParser parse];
+                                        } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, NSXMLParser *XMLParser) {
+                                            UIAlertView *av = [[UIAlertView alloc] initWithTitle:@"Error Retrieving Weather"
+                                                                                         message:[NSString stringWithFormat:@"%@",error]
+                                                                                        delegate:nil
+                                                                               cancelButtonTitle:@"OK"
+                                                                               otherButtonTitles:nil];
+                                            [av show];
+                                        }];
+    [operation start];
 }
 
 - (IBAction)httpClientTapped:(id)sender
@@ -139,23 +162,23 @@ static NSString *const BaseURLString = @"http://www.raywenderlich.com/downloads/
 
 /*
  The table view will have two sections: the first to display the current weather and the second to display the upcoming weather.
-*/
+ */
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     if (!self.weather) {
         return 0;
     }
-switch (section) {
-    case 0: {
-        return 1;
+    switch (section) {
+        case 0: {
+            return 1;
+        }
+        case 1: {
+            NSArray *upcomingWeather = [self.weather upcomingWeather];
+            return [upcomingWeather count];
+        }
+        default:
+            return 0;
     }
-    case 1: {
-        NSArray *upcomingWeather = [self.weather upcomingWeather];
-        return [upcomingWeather count];
-    }
-    default:
-        return 0;
-}
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -190,5 +213,77 @@ switch (section) {
 {
     // Navigation logic may go here. Create and push another view controller.
 }
+
+//The NSXMLParser calls this method when it finds a new element start tag. When that happens, you keep track of the previous element name before constructing a new dictionary for that section of the data in the currentDictionary property. You also reset the string outstring that you’ll build as you read the XML inside this tag.
+
+- (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary *)attributeDict {
+    if (qName) {
+        self.elementName = qName;
+    }
+    if ([qName isEqualToString:@"current_condition"]) {
+        self.currentDictionary = [NSMutableDictionary dictionary];
+    }
+    else if ([qName isEqualToString:@"weather"]) {
+        self.currentDictionary = [NSMutableDictionary dictionary];
+    }
+    else if([qName isEqualToString:@"request"]){
+        self.currentDictionary = [NSMutableDictionary dictionary];
+    }
+    self.outstring = [NSMutableString string];
+}
+
+//As the name suggests, you call this method when the parser finds character data while inside an XML tag. The method appends this character data to the outstring property, to be processed once the XML tag is closed.
+- (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string {
+    if (!self.elementName){
+        return;
+    }
+    
+    [self.outstring appendFormat:@"%@", string];
+}
+
+/*You call this method when you detect an element end tag. When that happens, you want to look for a few special tags:
+ The current_condition element means you have the weather for the current day. That can be added directly to the xmlWeather dictionary.
+ The weather element means you have the weather for a subsequent day. While there is only one current day, there are several subsequent days, so the weather here is added to an array.
+ The value tag appears inside other tags, so it’s safe to skip over it.
+ The weatherDesc and weatherIconUrl element values need to be boxed inside an array before they are stored to match how the JSON and plist versions are structured.
+ All other elements can be stored as-is.
+*/
+- (void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName {
+    if([qName isEqualToString:@"current_condition"] ||
+       [qName isEqualToString:@"request"]){
+        [self.xmlWeather setObject:[NSArray arrayWithObject:self.currentDictionary] forKey:qName];
+        self.currentDictionary = nil;
+    }
+    else if ([qName isEqualToString:@"weather"]) {
+        NSMutableArray *array = [self.xmlWeather objectForKey:@"weather"];
+        if (!array) {
+            array = [NSMutableArray array];
+        }
+        [array addObject:self.currentDictionary];
+        [self.xmlWeather setObject:array forKey:@"weather"];
+        self.currentDictionary = nil;
+    }
+    else if([qName isEqualToString:@"value"]){
+        //Ignore value tags they only appear in the two conditions below
+    }
+    else if([qName isEqualToString:@"weatherDesc"] ||
+            [qName isEqualToString:@"weatherIconUrl"]){
+        [self.currentDictionary setObject:[NSArray arrayWithObject:[NSDictionary dictionaryWithObject:self.outstring forKey:@"value"]] forKey:qName];
+    }
+    // 5
+    else {
+        [self.currentDictionary setObject:self.outstring forKey:qName];
+    }
+    
+	self.elementName = nil;
+
+}
+
+-(void) parserDidEndDocument:(NSXMLParser *)parser {
+    self.weather = [NSDictionary dictionaryWithObject:self.xmlWeather forKey:@"data"];
+    self.title = @"XML Retrieved";
+    [self.tableView reloadData];
+}
+
 
 @end
